704fe6bf22a0beec0ab0c3227bd8d8d71e53b8f3
João Vitor <joaovitorcarvalho903@gmail.com>
2025-08-02 16:01:26 -0300

    feat: configurando cadastro de clientes.

--- userprofile/models.py
+++ userprofile/models.py
@@ -2,9 +2,37 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class UserProfile(models.Model):
-    user= models.OneToOneField(
+    user = models.OneToOneField(
         User,
-        related_name='userprofile',
-        verbose_name='Usuário', 
-        on_delete=models.CASCADE)
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name='customers',
+        verbose_name='Usuário',
+    )
+    name = models.CharField(
+        max_length=100,
+    )
+    email = models.EmailField(
+        max_length=100,
+        blank='True',
+        null='True',
+        verbose_name='E-mail',
+    )
+    cpf = models.CharField(
+        max_length=15,
+        verbose_name='CPF'
+    )
+    phone = models.CharField(
+        max_length=15,
+        verbose_name='Telefone'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Criado em'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Atualizado em'
+    )
 
